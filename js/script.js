b5723a61a87815e4139d59492982b2d8ea720855
iamshakibb <shihabunshakib9346@gmail.com>
2020-08-12 23:21:31 +0600

    Update  some functionality

--- js/script.js
+++ js/script.js
@@ -2,81 +2,90 @@ window.addEventListener("load", () => {
   let longitude;
   let latitude;
   let time;
+
   if (navigator.geolocation) {
-    // getting longitude and latitude by geolocaion
-    navigator.geolocation.getCurrentPosition((position) => {
-      longitude = position.coords.longitude;
-      latitude = position.coords.latitude;
-      time = position.timestamp;
-      let localTimeFormat = new Date(time);
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        longitude = position.coords.longitude;
+        latitude = position.coords.latitude;
+        time = position.timestamp;
+        let localTimeFormat = new Date(time);
 
-      // weather api address and api key
-      const apiKey = "b15d2d0b7db4b947f2524705375669c7";
-      const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&dt=${time}&exclude=current,daily,hourly&appid=${apiKey}&units=metric`;
+        // weather api address and api key
+        const apiKey = "b15d2d0b7db4b947f2524705375669c7";
+        const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&dt=${time}&exclude=current,daily,hourly&appid=${apiKey}&units=metric`;
 
-      //calling the weather api
-      fetch(api)
-        .then((response) => {
-          let weather = response.json();
-          return weather;
-        })
-        .then((weather) => {
-          let temperature = document.querySelector(".temperature .temperature_number");
-          let temperatureDescription = document.querySelector(".temperature_description p");
-          let time = document.querySelector(".time p");
-          let icon = document.querySelector(".icon img");
+        //calling the weather api
+        fetch(api)
+          .then((response) => {
+            let weather = response.json();
+            return weather;
+          })
 
-          //getting the weather icon code
-          let iconCode = weather.current.weather[0].icon;
+          .then((weather) => {
+            let temperature = document.querySelector(".temperature .temperature_number");
+            let temperatureDescription = document.querySelector(".temperature_description p");
+            let time = document.querySelector(".time p");
+            let icon = document.querySelector(".icon img");
 
-          //getting the weather icon img
-          icon.setAttribute("src", `https://openweathermap.org/img/wn/${iconCode}@2x.png`);
-          temperature.textContent = weather.current.temp;
-          temperatureDescription.textContent = weather.current.weather[0].main;
-          time.textContent = localTimeFormat.toLocaleString();
-          sevenDaysWeather();
-          slider();
+            //getting the weather icon code
+            let iconCode = weather.current.weather[0].icon;
 
-          //function for creating dynamic div of daily weather
-          function sevenDaysWeather() {
-            let daysWeather = document.querySelector(".days_weather .container");
-            for (let i = 1; i < weather.daily.length; i++) {
-              let date = new Date(weather.daily[i].dt * 1000).toLocaleDateString();
-              let iconCode = weather.daily[i].weather[0].icon;
-              let dailyWeatherDescription = weather.daily[i].weather[0].main;
-              let dailyTemperature = weather.daily[i].temp.max;
-              let dailyWeatherDiv = document.createElement("div");
-              dailyWeatherDiv.classList.add("text-center");
-              dailyWeatherDiv.innerHTML += `<h4>${date}</h4>
+            //getting the weather icon img
+            icon.setAttribute("src", `https://openweathermap.org/img/wn/${iconCode}@2x.png`);
+            temperature.textContent = weather.current.temp;
+            temperatureDescription.textContent = weather.current.weather[0].main;
+            time.textContent = localTimeFormat.toLocaleString();
+            sevenDaysWeather();
+            slider();
+
+            //function for creating dynamic div of daily weather
+            function sevenDaysWeather() {
+              let daysWeather = document.querySelector(".days_weather .container");
+              for (let i = 1; i < weather.daily.length; i++) {
+                let date = new Date(weather.daily[i].dt * 1000).toLocaleDateString();
+                let iconCode = weather.daily[i].weather[0].icon;
+                let dailyWeatherDescription = weather.daily[i].weather[0].main;
+                let dailyTemperature = weather.daily[i].temp.max;
+                let dailyWeatherDiv = document.createElement("div");
+                dailyWeatherDiv.classList.add("text-center");
+                dailyWeatherDiv.innerHTML += `<h4>${date}</h4>
                                            <img class="center-block" src="https://openweathermap.org/img/wn/${iconCode}@2x.png" alt="Weather Icon" />
                                            <h5>${dailyTemperature} <span>C</span></h5>
                                            <p>${dailyWeatherDescription}</p>`;
-              daysWeather.appendChild(dailyWeatherDiv);
+                daysWeather.appendChild(dailyWeatherDiv);
+              }
             }
-          }
-        });
+          });
 
-      //location api address
-      const location_api = `https://ipapi.co/json/`;
+        //location api address
+        const location_api = `https://ipapi.co/json/`;
 
-      //calling the location api
-      fetch(location_api)
-        .then((response) => {
-          let date = response.json();
-          return date;
-        })
-        .then((data) => {
-          let location = document.querySelector(".location h2");
-          location.textContent = data.city;
-        });
-    });
-  } else {
-    alert("PLEASE ALLOW YOUR LOCATION");
+        //calling the location api
+        fetch(location_api)
+          .then((response) => {
+            let date = response.json();
+            return date;
+          })
+          .then((data) => {
+            let location = document.querySelector(".location h2");
+            location.textContent = data.city;
+          });
+      },
+
+      //if user not allow to access location
+      (error) => {
+        let message = document.querySelector(".error");
+        let success = document.querySelector(".success");
+        message.classList.remove("error_style");
+        message.classList.add("show_message");
+        success.style.display = "none";
+      }
+    );
   }
 });
 
 // slider section
-
 function slider() {
   $(document).ready(function () {
     $(".days_weather .container").slick({
